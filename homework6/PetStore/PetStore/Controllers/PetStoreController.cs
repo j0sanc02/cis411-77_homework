515953ccc61cb7ad008c7a973736f47a7d0cfbbf
j0sanc02 <j0sanc02@louisville.edu>
2014-03-13 21:22:52 -0400

    Homework 6 Initial Commit

---
+++ homework6/PetStore/PetStore/Controllers/PetStoreController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetStore.Controllers
+{
+    public class PetStoreController : Controller
+    {
+        //
+        // GET: /PetStore/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ homework6/PetStore/PetStore/Models/Animals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;   
+
+namespace PetStore.Models
+{
+    public class Animals
+    {
+        public string name { get; set; }
+        public string breed { get; set; }
+        public DateTime DOB { get; set; }
+        public string sex { get; set; }
+        public Boolean spade { get; set; }
+
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Animals> Animal { get; set; }
+    }
+}
