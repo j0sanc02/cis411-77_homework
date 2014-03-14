0e319a41f2cdce4663b4b1a437d0ce10d2cd28f0
j0sanc02 <j0sanc02@louisville.edu>
2014-03-13 22:11:24 -0400

    Homework 6 2nd Commit

---
+++ homework6/PetStore/PetStore/Controllers/AnimalController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PetStore.Models;
+
+namespace PetStore.Controllers
+{
+    public class AnimalController : Controller
+    {
+        private PetDBContext db = new PetDBContext();
+
+        // GET: /Animal/
+        public ActionResult Index()
+        {
+            return View(db.Animal.ToList());
+        }
+
+        // GET: /Animal/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animals animals = db.Animal.Find(id);
+            if (animals == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animals);
+        }
+
+        // GET: /Animal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Animal/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Name,breed,DOB,sex,spade")] Animals animals)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Animal.Add(animals);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(animals);
+        }
+
+        // GET: /Animal/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animals animals = db.Animal.Find(id);
+            if (animals == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animals);
+        }
+
+        // POST: /Animal/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Name,breed,DOB,sex,spade")] Animals animals)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(animals).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(animals);
+        }
+
+        // GET: /Animal/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animals animals = db.Animal.Find(id);
+            if (animals == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animals);
+        }
+
+        // POST: /Animal/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Animals animals = db.Animal.Find(id);
+            db.Animal.Remove(animals);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
