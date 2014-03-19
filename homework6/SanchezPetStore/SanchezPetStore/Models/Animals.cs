870390ddfbeb28e7dd00dd1c3c946acebd4f02c3
j0sanc02 <j0sanc02@louisville.edu>
2014-03-18 23:10:46 -0400

    Homework 6 Commit after reading the instructions

---
+++ homework6/SanchezPetStore/SanchezPetStore/Models/Animals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace SanchezPetStore.Models
+{
+    public class Animals
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateReceived { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+    }
+
+    public class AnimalDBContext : DbContext
+    {
+        public DbSet<Animals> Animal { get; set; }
+    }
+}
